51475c80b8a7f03e72f949263f48641f5129df83
László heim <lasolozdev@gmail.com>
2019-12-26 20:50:24 +0200

    [feat] Handle key events on text field

--- qpt-gui/src/main/kotlin/com/lasoloz/tools/qpt/gui/launcher/component/LaunchBarController.kt
+++ qpt-gui/src/main/kotlin/com/lasoloz/tools/qpt/gui/launcher/component/LaunchBarController.kt
@@ -7,16 +7,25 @@ import com.lasoloz.tools.qpt.gui.state.LauncherState
 import com.lasoloz.tools.qpt.injections.InjectorUtil
 import javafx.fxml.FXML
 import javafx.fxml.Initializable
-import javafx.scene.control.ToolBar
+import javafx.scene.input.KeyCode
+import javafx.scene.input.KeyEvent
 import java.net.URL
 import java.util.*
 
 class LaunchBarController : Initializable {
     private lateinit var launcherState: LauncherState
 
+    @FXML
+    private lateinit var textField: PersistentPromptTextField
+
     @Inject
     fun setLauncherState(launcherState: LauncherState) {
         this.launcherState = launcherState
+    }
+
+    @FXML
+    private fun keyOnReleaseAction(e: KeyEvent) {
+        println("TODO: Look up text value ${textField.text}")
     }
 
     @FXML
@@ -26,5 +35,28 @@ class LaunchBarController : Initializable {
 
     override fun initialize(location: URL?, resources: ResourceBundle?) {
         InjectorUtil.getInjector().injectMembers(this)
+        addUpDownFilter()
+    }
+
+    private fun addUpDownFilter() {
+        textField.addEventFilter(KeyEvent.ANY) { event ->
+            if (event.code == KeyCode.UP) {
+                event.doOnRelease {
+                    println("TODO: Select previous")
+                }
+                event.consume()
+            } else if (event.code == KeyCode.DOWN) {
+                event.doOnRelease {
+                    println("TODO: Select next")
+                }
+                event.consume()
+            }
+        }
+    }
+
+    private fun KeyEvent.doOnRelease(action: () -> Unit) {
+        if (eventType == KeyEvent.KEY_RELEASED) {
+            action()
+        }
     }
 }
